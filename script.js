1f3b24f5ce4d741cf6ad1ea852fc0f7fb0af1b0a
GeoGeo116 <68518082+GeoGeo116@users.noreply.github.com>
2025-04-08 19:02:13 +0100

    Updated the display
    Display now shows the most recent button press (Obviously not fully functional yet)
    Can no longer select text (stops selection when clicking the buttons fast)
    Slightly modified the display size
    Added an id to each button with corresponding value

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function (){
     var buttons = document.querySelectorAll("td");
-    function playClick(){
+    function buttonPress(){
         console.log("Playing");
         var audio = new Audio("./sound/click.mp3");
         audio.play();
+
+        document.getElementById("display").innerHTML = event.target.id
     }
     
     for (var btn of buttons){
-        btn.onclick = playClick;
+        btn.onclick = buttonPress;
     }
 })
